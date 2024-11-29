e0037bb3fa35c7596419a7d67843f9eb8486d8a2
Bruno Mendonça <103976296+brunomendoncadev@users.noreply.github.com>
2024-11-28 22:40:41 -0300

    Adicionado tela de cadastro de usuario.

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './src/pages/login';
+import Signup from './src/pages/signup';
 
 export default function App() {
   return (
-      <Login/>
+      //<Login/>//
+      <Signup/>
   );
 }
 
