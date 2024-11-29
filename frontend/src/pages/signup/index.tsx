f32786b4d1bcf7bda2293ca6b3d0eb8c405ed595
beatriz-mattos <bmattos@outlook.de>
2024-11-29 18:47:10 -0300

    added sign up navigation

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,34 +1,29 @@
 import React, { useState } from 'react';
-import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View, Platform } from 'react-native';
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles'
 import Logo from "../../assets/logo.png";
 
-export default function Signup() {
+export default function Signup({ navigation }: { navigation: any }) {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  //const router = useRouter();
 
   const handleSignup = () => {
-    // Aqui você pode adicionar a lógica para autenticação do usuário
-    console.log('Nome:', nome);
-    console.log('Email:', email);
-    console.log('Password:', password);
-    //router.push('/MenuScreen');
+    navigation.navigate('Login');
   };
 
   return (
     <View style={styles.container}>
       <Image
-                source={Logo}
-                style={styles.logo}
-                resizeMode="contain"
-            />
+        source={Logo}
+        style={styles.logo}
+        resizeMode="contain"
+      />
       <Text style={styles.subtitle}>Cadastre-se para acessar</Text>
 
 
       <View style={styles.inputContainer}>
-      <TextInput 
+        <TextInput
           style={styles.input}
           placeholder="Nome"
           value={nome}
@@ -55,9 +50,8 @@ export default function Signup() {
       </View>
 
       <TouchableOpacity onPress={handleSignup} style={styles.button}>
-      <Text style={styles.textButton}>Cadastro</Text>
+        <Text style={styles.textButton}>Finalizar cadastro e acessar</Text>
       </TouchableOpacity>
     </View>
   );
 }
-
