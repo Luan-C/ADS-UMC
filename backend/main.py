b344ad915cb51253073da4710d4041b492e4135c
beatriz-mattos <bmattos@outlook.de>
2024-11-29 18:39:57 -0300

    enable CORS on backend

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from router import user, car
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.register_blueprint(user)
 app.register_blueprint(car)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
